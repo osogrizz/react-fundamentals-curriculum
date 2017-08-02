7bb516b524e81191a58739d27d2ef9a7bd13fefb
Leo Torres <leo.htor@gmail.com>
2017-08-02 16:56:58 -0400

    added UI to Nav

--- app/Nav.js
+++ app/Nav.js
@@ -4,9 +4,27 @@ export class Nav extends React.Component {
   render() {
     return (
       <div className='mdl-layout mdl-js-layout'>
+
         <header className='mdl-layout__header mdl-layout__header--scroll'>
           <div className='mdl-layout__header-row'>
             <span className='mdl-layout-title'>BY JUPITER!</span>
+            <div className='mdl-layout-spacer'></div>
+
+            <div className='mdl-textfield mdl-js-textfield mdl-textfield--expandable
+                  mdl-textfield--floating-label mdl-textfield--align-right'>
+
+            <label className='mdl-button mdl-js-button mdl-button--icon'
+                   htmlFor='fixed-header-drawer-exp'>
+              <i className="material-icons">search</i>
+            </label>
+            <div className='mdl-textfield__expandable-holder'>
+              <input className="mdl-textfield__input" type="text" name="sample"
+                 id="fixed-header-drawer-exp" placeholder='Get Weather' />
+            </div>
+
+            </div>
+
+
           </div>
         </header>
       </div>
